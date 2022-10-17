6b3ab0f99f213bedfbd16d15180c4b9d96a537d5
aswinblix <aswin.blix@bitsnpixs.com>
2022-10-17 16:43:42 +0530

    add user, post & category schema with fake data

--- database/migrations/2022_10_17_105503_create_posts_table.php
+++ database/migrations/2022_10_17_105503_create_posts_table.php
@@ -13,14 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id');
+            $table->foreignId('category_id');
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+            $table->longText('excerpt');
+            $table->longText('body');
+            $table->integer('views')->default(0);
+            $table->string('thumbnail');
             $table->timestamps();
         });
     }
@@ -32,6 +34,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
