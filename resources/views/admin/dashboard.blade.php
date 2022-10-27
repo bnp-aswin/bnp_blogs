40495a7246fb42e1b9ef7c4a98aa47ed12f5fd69
aswinblix <aswin.blix@bitsnpixs.com>
2022-10-27 14:41:43 +0530

    add verify user by role

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.default')
+@section('content')
+    <div class="container"></div>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h1 class="text-center">Welcome admin</h1>
+        </div>
+    </div>
+
+@endsection
