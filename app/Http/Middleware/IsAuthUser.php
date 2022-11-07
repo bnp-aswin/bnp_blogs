2c8070a1511285aff1908f6eb73af978eae33e68
aswinblix <aswin.blix@bitsnpixs.com>
2022-11-07 11:06:27 +0530

    add custom middleware

---
+++ app/Http/Middleware/IsAuthUser.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class IsAuthUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next, $role)
+    {
+        if (auth()->check() && auth()->user()->role_id != $role) {
+
+            return redirect()->route('home')->with('status', 'UnAthorized user for this page');
+        }
+        return $next($request);
+    }
+}
